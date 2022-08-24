ad4583de6e55178bc297ca2df0e0984e8f2ad5f4
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-25 00:24:57 +0600

    Add files via upload

---
+++ swap1.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main(void)
+{
+    int num1=3,num2=8,temp;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    printf("num1=%d\n",num1);
+   printf("num2=%d",num2);
+    return 0;}
